2aa3461584a59fbecb31bdadd16e7ed40723db14
Iain <iain@iainferguson.org>
2020-05-27 21:33:46 +0100

    finalised etch-a-sketch

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const resetButton = document.getElementById("reset");
 const colorButton = document.getElementById("colorButton");
+const gridSize = document.getElementById("numberGrid");
+const submitButton = document.getElementById("submitButton");
+const divContainer = document.getElementById("container");
 let colorType = "black";
-let gridNumber = '';
-let r = Math.floor(Math.random() * 256);
-let g = Math.floor(Math.random() * 256);
-let b = Math.floor(Math.random() * 256);
-let rgbColor = "rgb(" + r + "," + g + "," + b + ")";
+
+
+// creates a num*num grid, sets rows and columns based on num value
 
 function createGrid(num){
-    if (gridNumber == '') {
-        gridNumber = (16 ** 2);
-    } else {
-        gridnumber = (num ** 2);
-    }
+
+    let gridNumber = (num ** 2);
+
+    divContainer.style.setProperty("--numRows", num);
+    divContainer.style.setProperty("--numCols", num);
 
     for (i = 0; i < gridNumber; i++) {
         let grid = document.createElement("div");
@@ -24,10 +25,10 @@ function createGrid(num){
 
 }
 
-document.onload = createGrid();
+// cycles through each grid element and sets the color to white
 
 function resetGrid() {
-    gridSelect.forEach(grid => grid.style.backgroundColor = "white");
+    document.querySelectorAll(".grid").forEach(grid => grid.style.backgroundColor = "white");
     colorType = "black";
 }
 
@@ -35,17 +36,35 @@ function changeColor(e) {
     e.target.style.backgroundColor = colorType;
 }
 
-const gridSelect = document.querySelectorAll(".grid");
+//creates a random RGB value and sets it to the current color
 
-gridSelect.forEach(grid => grid.addEventListener("mouseover", changeColor));
-resetButton.addEventListener("click", resetGrid);
-colorButton.addEventListener("click", function() {
+function randomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let rgbColor = "rgb(" + r + "," + g + "," + b + ")";
     colorType = rgbColor;
     changeColor;
+}
+
+resetButton.addEventListener("click", resetGrid);
+
+colorButton.addEventListener("click", randomColor);
+
+submitButton.addEventListener("click", function() {
+    while(divContainer.lastElementChild) {
+        divContainer.removeChild(divContainer.lastElementChild);
+    };
+    createGrid(parseInt(gridSize.value));
+    resetGrid;
+    const gridSelect = document.querySelectorAll(".grid");
+    gridSelect.forEach((grid) => grid.addEventListener("mouseover", changeColor));
 });
+
+document.onload = createGrid(16);
+const gridSelect = document.querySelectorAll(".grid");
+gridSelect.forEach((grid) => grid.addEventListener("mouseover", changeColor));
+
+
 
 
